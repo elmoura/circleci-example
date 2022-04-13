6309801cbbcb1c40c16193226f6bf50a3ca3272d
Gabriel Moura <mourasb.gabriel@gmail.com>
2022-04-13 17:16:13 -0300

    feat: adicionar cache do yarn no CircleCI

--- src/services/__tests__/app-service.spec.ts
+++ src/services/__tests__/app-service.spec.ts
@@ -5,7 +5,6 @@ const appService = new AppService()
 describe('AppService tests', () => {
   test('should return a hello world message', () => {
     const { message } = appService.helloMessage()
-
-    expect(message).toBeFalsy()
+    expect(message).toBeTruthy()
   })
 })
